6fd73b534fbf7750748d82d6044e4f0f056ffd96
Sahil Vazirani <svazirani@salesforce.com>
2020-11-12 18:07:19 -0800

    initial commit

--- policy_sentry/provider.go
+++ policy_sentry/provider.go
@@ -1,12 +1,22 @@
 package policy_sentry
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	policySentryRest "terraform-provider-policy-sentry/policy_sentry_rest"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+// Provider returns a *schema.Provider.
 func Provider() *schema.Provider {
 	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"endpoint": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: descriptions["endpoint"],
+			},
+		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"policy-sentry_document": dataSourcePolicySentryDocument(),
 		},
@@ -20,5 +30,13 @@ func configureFunc() func(*schema.ResourceData) (interface{}, error) {
 	return func(d *schema.ResourceData) (interface{}, error) {
 		client := policySentryRest.NewClient()
 		return client, nil
+	}
+}
+
+var descriptions map[string]string
+
+func init() {
+	descriptions = map[string]string{
+		"endpoint": "Use this to override the default service endpoint URL",
 	}
 }
