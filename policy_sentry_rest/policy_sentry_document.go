dee9b4869c71d529d09cb81e0ebf498a71aca0b0
reetasingh <reetasingh@salesforce.com>
2020-11-22 14:04:36 -0800

    fix bug

--- policy_sentry_rest/policy_sentry_document.go
+++ policy_sentry_rest/policy_sentry_document.go
@@ -2,48 +2,50 @@ package policy_sentry_rest
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 const PolicyDocumentPath string = "write"
-const DefaultRestUrl string = "https://tpzr780iv0.execute-api.us-east-1.amazonaws.com/dev/"
+const DefaultRestUrl string = "https://rycbfaz4wl.execute-api.us-east-1.amazonaws.com/dev/"
 
 func (c *Client) GetPolicyDocument(input *PolicyDocumentInput) (*PolicyDocument, error) {
 	var policyDocument PolicyDocument
 
 	var inputBody map[string]interface{} = make(map[string]interface{})
 
-	if len(input.ActionsForResources.Read) > 0 {
-		inputBody["read"] = input.ActionsForResources.Read
+	if len(input.ActionsForServices.Read) > 0 {
+		inputBody["read"] = input.ActionsForServices.Read
 	}
-	if len(input.ActionsForResources.Write) > 0 {
-		inputBody["write"] = input.ActionsForResources.Write
+	if len(input.ActionsForServices.Write) > 0 {
+		inputBody["write"] = input.ActionsForServices.Write
 	}
-	if len(input.ActionsForResources.Tagging) > 0 {
-		inputBody["tagging"] = input.ActionsForResources.Tagging
+	if len(input.ActionsForServices.Tagging) > 0 {
+		inputBody["tagging"] = input.ActionsForServices.Tagging
 	}
-	if len(input.ActionsForResources.PermissionsManagement) > 0 {
-		inputBody["permissions-management"] = input.ActionsForResources.PermissionsManagement
+	if len(input.ActionsForServices.PermissionsManagement) > 0 {
+		inputBody["permissions-management"] = input.ActionsForServices.PermissionsManagement
 	}
-	if len(input.ActionsForResources.List) > 0 {
-		inputBody["list"] = input.ActionsForResources.List
+	if len(input.ActionsForServices.List) > 0 {
+		inputBody["list"] = input.ActionsForServices.List
 	}
+
 	if len(input.ActionsForResources.SingleActions) > 0 {
 		inputBody["single-actions"] = input.ActionsForResources.SingleActions
 	}
-	if len(input.ActionsForServices.Read) > 0 {
-		inputBody["service-read"] = input.ActionsForServices.Read
+	if len(input.ActionsForResources.Read) > 0 {
+		inputBody["service-read"] = input.ActionsForResources.Read
 	}
-	if len(input.ActionsForServices.Write) > 0 {
-		inputBody["service-write"] = input.ActionsForServices.Write
+	if len(input.ActionsForResources.Write) > 0 {
+		inputBody["service-write"] = input.ActionsForResources.Write
 	}
-	if len(input.ActionsForServices.Tagging) > 0 {
-		inputBody["service-tagging"] = input.ActionsForServices.Tagging
+	if len(input.ActionsForResources.Tagging) > 0 {
+		inputBody["service-tagging"] = input.ActionsForResources.Tagging
 	}
-	if len(input.ActionsForServices.PermissionsManagement) > 0 {
-		inputBody["service-permissions-management"] = input.ActionsForServices.PermissionsManagement
+	if len(input.ActionsForResources.PermissionsManagement) > 0 {
+		inputBody["service-permissions-management"] = input.ActionsForResources.PermissionsManagement
 	}
-	if len(input.ActionsForServices.List) > 0 {
-		inputBody["service-list"] = input.ActionsForServices.List
+	if len(input.ActionsForResources.List) > 0 {
+		inputBody["service-list"] = input.ActionsForResources.List
 	}
 
 	if len(input.Overrides.SkipResourceConstraints) > 0 {
@@ -53,6 +55,8 @@ func (c *Client) GetPolicyDocument(input *PolicyDocumentInput) (*PolicyDocument,
 	if len(input.ExcludeActions) > 0 {
 		inputBody["exclude-actions"] = input.ExcludeActions
 	}
+
+	fmt.Println(inputBody)
 
 	requestBody, err := json.Marshal(inputBody)
 
