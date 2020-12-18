64ade0e9e4edbbeaea8c6bafb008599c04ad22cd
Kinnaird McQuade <kmcquade@salesforce.com>
2020-12-18 09:46:09 -0500

    Fix version requirements. Debugging internal server error

--- examples/main.tf
+++ examples/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     policyguru = {
       source  = "salesforce/policyguru"
-      version = "1.3.1"
+      version = "2.0.0"
     }
     aws = {
       source  = "hashicorp/aws"
