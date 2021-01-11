e6157d316821e68587ea18fdd8bbadb63fa639d8
reetasingh <reetasingh@salesforce.com>
2021-01-10 17:39:11 -0800

    add unit tests

--- policyguru/expand_test.go
+++ policyguru/expand_test.go
@@ -1,7 +1,6 @@
 package policyguru
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -26,8 +25,6 @@ func TestExpandActionForResourcesAtAccessLevel(t *testing.T) {
 	input := make([]interface{}, 0, 1)
 	input = append(input, inputMap)
 
-	fmt.Println(input)
-
 	actionForResources, err := expandActionForResourcesAtAccessLevel(input)
 	if err != nil {
 		t.Fatal(err)
@@ -37,5 +34,60 @@ func TestExpandActionForResourcesAtAccessLevel(t *testing.T) {
 	assertNotNil(t, actionForResources.List)
 	assertNotNil(t, actionForResources.Tagging)
 	assertNotNil(t, actionForResources.PermissionsManagement)
+
+}
+
+func TestExpandActionForServicesWithoutResourceConstraints(t *testing.T) {
+
+	inputMap := make(map[string]interface{})
+
+	var data []interface{}
+	data = append(data, "s3")
+	inputMap["read"] = data
+	inputMap["write"] = data
+	inputMap["tagging"] = data
+	inputMap["permissions_management"] = data
+	inputMap["list"] = data
+
+	input := make([]interface{}, 0, 1)
+	input = append(input, inputMap)
+
+	actionForServices, err := expandActionForServicesWithoutResourceConstraints(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertNotNil(t, actionForServices.Read)
+	assertNotNil(t, actionForServices.Write)
+	assertNotNil(t, actionForServices.List)
+	assertNotNil(t, actionForServices.Tagging)
+	assertNotNil(t, actionForServices.PermissionsManagement)
+
+}
+
+func TestExpandOverrides(t *testing.T) {
+
+    inputMap := make(map[string]interface{})
+
+	var data []interface{}
+	data = append(data, "s3")
+	inputMap["skip_resource_constraints_for_actions"] = data
+
+	input := make([]interface{}, 0, 1)
+	input = append(input, inputMap)
+
+	overrides , err := expandOverrides(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertNotNil(t, overrides.SkipResourceConstraints)
+}
+
+func TestExpandStringList(t *testing.T) {
+
+    input := make([]interface{}, 0, 1)
+	input = append(input, "abc")
+	result := expandStringList(input)
+
+	assertNotNil(t, result)
 
 }
